d1c95355ee832883d54b4f623c19fb2f70d15f88
Simon Moses <simon@seamlesshr.com>
2022-11-02 16:43:38 +0100

    add dependencies jest lint prettier

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server')
+})
+
+// should return hello world
+app.get('/hello', (req: Request, res: Response) => {
+  res.send('Hello world')
+})
+
+export default app
